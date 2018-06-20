3532a544535acf316eca35757b15285716d3f68c
Paul <pkaplan@media.mit.edu>
2018-06-20 02:56:08 +0000

    Build for a260bebb454a83e5249e921f5965fd6f89a11bfa

--- develop/blocksonly.js
+++ develop/blocksonly.js
@@ -1,6 +1,50 @@
-webpackJsonp([2],{
+var GUI =
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[3],{
 
-/***/ 504:
+/***/ 416:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(4)(false);
+// imports
+
+
+// module
+exports.push([module.i, ".blocks-only_controls_2GEvs {\n    position: absolute;\n    z-index: 2;\n    top: 10px;\n    right: 15px;\n}\n", ""]);
+
+// exports
+exports.locals = {
+	"controls": "blocks-only_controls_2GEvs"
+};
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(416);
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(3)(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
+/***/ 418:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -10,40 +54,40 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = __webpack_require__(12);
+var _reactDom = __webpack_require__(32);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
 var _reactRedux = __webpack_require__(7);
 
-var _appStateHoc = __webpack_require__(21);
-
-var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
-
-var _controls = __webpack_require__(29);
+var _controls = __webpack_require__(249);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _blocks = __webpack_require__(94);
+var _blocks = __webpack_require__(320);
 
 var _blocks2 = _interopRequireDefault(_blocks);
 
-var _gui = __webpack_require__(24);
+var _gui = __webpack_require__(151);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(30);
+var _hashParserHoc = __webpack_require__(116);
 
-var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
+var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
 
-var _blocksOnly = __webpack_require__(505);
+var _appStateHoc = __webpack_require__(115);
+
+var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
+
+var _blocksOnly = __webpack_require__(417);
 
 var _blocksOnly2 = _interopRequireDefault(_blocksOnly);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mapStateToProps = function mapStateToProps(state) {
-    return { vm: state.vm };
+    return { vm: state.scratchGui.vm };
 };
 
 var VMBlocks = (0, _reactRedux.connect)(mapStateToProps)(_blocks2.default);
@@ -63,82 +107,14 @@ var BlocksOnly = function BlocksOnly(props) {
     );
 };
 
-var App = (0, _appStateHoc2.default)((0, _projectLoaderHoc2.default)(BlocksOnly));
+var App = (0, _hashParserHoc2.default)((0, _appStateHoc2.default)(BlocksOnly));
 
 var appTarget = document.createElement('div');
 document.body.appendChild(appTarget);
 
 _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
-/***/ }),
-
-/***/ 505:
-/***/ (function(module, exports, __webpack_require__) {
-
-
-var content = __webpack_require__(506);
-
-if(typeof content === 'string') content = [[module.i, content, '']];
-
-var transform;
-var insertInto;
-
-
-
-var options = {"hmr":true}
-
-options.transform = transform
-options.insertInto = undefined;
-
-var update = __webpack_require__(3)(content, options);
-
-if(content.locals) module.exports = content.locals;
-
-if(false) {
-	module.hot.accept("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./blocks-only.css", function() {
-		var newContent = require("!!../../node_modules/css-loader/index.js??ref--1-1!../../node_modules/postcss-loader/lib/index.js??postcss!./blocks-only.css");
-
-		if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-
-		var locals = (function(a, b) {
-			var key, idx = 0;
-
-			for(key in a) {
-				if(!b || a[key] !== b[key]) return false;
-				idx++;
-			}
-
-			for(key in b) idx--;
-
-			return idx === 0;
-		}(content.locals, newContent.locals));
-
-		if(!locals) throw new Error('Aborting CSS HMR due to changed css-modules locals.');
-
-		update(newContent);
-	});
-
-	module.hot.dispose(function() { update(); });
-}
-
-/***/ }),
-
-/***/ 506:
-/***/ (function(module, exports, __webpack_require__) {
-
-exports = module.exports = __webpack_require__(2)(false);
-// imports
-
-
-// module
-exports.push([module.i, ".blocks-only_controls_1gHLl {\n    position: absolute;\n    z-index: 2;\n    top: 10px;\n    right: 15px;\n}\n", ""]);
-
-// exports
-exports.locals = {
-	"controls": "blocks-only_controls_1gHLl"
-};
-
 /***/ })
 
-},[504]);
+},[[418,0]]]);
 //# sourceMappingURL=blocksonly.js.map
